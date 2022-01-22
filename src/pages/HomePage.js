d4e0404c02d909f1ef1b6bcbc8af55f209bbb13f
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-22 11:01:18 +0700

    adding hero section and just arrived section

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import JustArrived from "parts/HomePage/JustArrived";
+
+function HomePage(props) {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <JustArrived />
+    </>
+  );
+}
+
+export default HomePage;
