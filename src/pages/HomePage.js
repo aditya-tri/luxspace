190aafa393eae90e8550e0f0ad7e534a05f33938
Aditya <adityatrisaktisilalahi@gmail.com>
2022-01-22 11:33:56 +0700

    create client footer sitemap browseroom components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,21 @@
+import Clients from "parts/Clients";
+import Footer from "parts/Footer";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
+import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
+import SiteMap from "parts/SiteMap";
 
 function HomePage(props) {
   return (
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
       <JustArrived />
+      <Clients />
+      <SiteMap />
+      <Footer />
     </>
   );
 }
