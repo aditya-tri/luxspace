0d744a10ae0ac16a31a06fe2f1eaf57e8e189bc4
Aditya <adityatrisaktisilalahi@gmail.com>
2022-03-07 13:23:19 +0700

    changing path for detail product

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/categories/:idc" element={<Details />} />
+          <Route path="/categories/:idc/products/:idp" element={<Details />} />
           <Route path="/congratulation" element={<Congratulation />} />
           <Route path="*" element={<Notfound />} />
         </Routes>
