25fff0a8ba83776ca3e3c2027da43d56f54af70f
Aditya <adityatrisaktisilalahi@gmail.com>
2022-03-08 09:43:55 +0700

    fetching api for product details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import Breadcrumb from "components/Breadcrumb";
+
 import Clients from "parts/Clients";
 import ProductDetails from "parts/Details/ProductDetails";
 import Suggestion from "parts/Details/Suggestion";
@@ -6,7 +10,22 @@ import Footer from "parts/Footer";
 import Header from "parts/Header";
 import SiteMap from "parts/SiteMap";
 
+import useAsync from "helpers/hooks/useAsync";
+import fetch from "helpers/fetch";
+
 function Details() {
+  // hook untuk mengambil parameter yang ada di link
+  const { idp } = useParams();
+  console.log(idp);
+
+  const { data, run, isLoading } = useAsync();
+
+  // fetch api menggunakan fetch custom yang telah di buat
+  useEffect(() => {
+    run(fetch({ url: `/api/products/${idp}` }));
+  }, [run, idp]);
+
+  console.log(data);
   return (
     <div className="mx-7">
       <Header theme="black" />
