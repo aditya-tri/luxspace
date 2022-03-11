109f766fb64ecefb2406aeabe98b1dff69a1b2de
Aditya <adityatrisaktisilalahi@gmail.com>
2022-03-11 10:52:45 +0700

    adding async await method

--- src/helpers/fetch/index.js
+++ src/helpers/fetch/index.js
@@ -1,23 +1,21 @@
-// Membuat helper fetch untuk api agar lebih cepat saat memanggil api
+//helper fetch untuk api agar lebih cepat saat memanggil api di tiap component
 
-export default function fetchData({
+export default async function FetchData({
   url,
   method = "GET",
   host = process.env.REACT_APP_API_HOST,
   body,
 }) {
-  return fetch(`${host}${url}`, {
+  const response = await fetch(`${host}${url}`, {
     method,
     mode: "cors",
     headers: { "Content-Type": "application/json" },
     body,
-  }).then(async (response) => {
-    const statusHasResponse = [200, 404];
-    const jsonResponse = statusHasResponse.includes(response.status)
-      ? await response.json()
-      : response;
-    if (response.ok) return jsonResponse;
-
-    throw new Error(JSON.stringify(jsonResponse));
   });
+  const statusHasResponse = [200, 404];
+  const jsonResponse = statusHasResponse.includes(response.status)
+    ? await response.json()
+    : response;
+  if (response.ok) return jsonResponse;
+  throw new Error(JSON.stringify(jsonResponse));
 }
