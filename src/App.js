cc4edfbaca9b4aeee84fa2054351e5933670c039
Aditya <adityatrisaktisilalahi@gmail.com>
2022-03-14 09:22:48 +0700

    adding splash screen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "assets/css/app.css";
 
 import HomePage from "pages/HomePage";
@@ -7,21 +8,35 @@ import Congratulation from "pages/Congratulation";
 import Notfound from "pages/Notfound";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Provider from "helpers/hooks/useGlobalContext";
+import Splash from "components/Splash";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1500);
   return (
-    <Provider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/categories/:idc" element={<Details />} />
-          <Route path="/categories/:idc/products/:idp" element={<Details />} />
-          <Route path="/congratulation" element={<Congratulation />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      {isLoading ? (
+        <Splash />
+      ) : (
+        <Provider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/categories/:idc" element={<Details />} />
+              <Route
+                path="/categories/:idc/products/:idp"
+                element={<Details />}
+              />
+              <Route path="/congratulation" element={<Congratulation />} />
+              <Route path="*" element={<Notfound />} />
+            </Routes>
+          </BrowserRouter>
+        </Provider>
+      )}
+    </>
   );
 }
 
